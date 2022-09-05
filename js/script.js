82873d84afda0fe3042eb2df30df0821bad2efe8
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2022-09-05 18:52:27 -0300

    teste
    teste

--- js/script.js
+++ js/script.js
@@ -19,3 +19,18 @@ if (hora >= 0 && hora < 12) {
 }
 
 }
+
+// -------------- Segunda Section----------//
+
+function contar(){
+    let ini = document.getElementById('txti') // estou pegando as inf dessa id
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    window.alert('[ERRO] Faltam Dados')
+
+} else {
+    window.alert('Tudo Ok')
+}
+}
