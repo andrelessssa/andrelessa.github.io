820f00a8f8502011e51b4e2acd68e49025baf896
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2022-09-23 17:32:40 -0300

    code
    code

--- js/jogo.js
+++ js/jogo.js
@@ -1,47 +1,39 @@
-const mario = document.querySelector('.mario'); // acessando a class do mario
-const pipe = document.querySelector('.pipe');
-const fim = document.querySelector('.fim');
-
-
+const mario = document.querySelector(".mario"); // acessando a class do mario
+const pipe = document.querySelector(".pipe");
+const fim = document.querySelector(".fim");
 
 const jump = () => {
-  mario.classList.add('jump');
-  
+  mario.classList.add("jump");
+
   setTimeout(() => {
-    mario.classList.remove('jump');
-  },500)
-}
-const loop = setInterval ( () => {
-  
-    console.log("loop");
-    fim.src = 'none';
+    mario.classList.remove("jump");
+  }, 500);
+};
+const loop = setInterval(() => {
+  console.log("loop");
+  fim.src = "none";
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); // com esse + ele ja converte pra numero
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", ""); // com esse + ele ja converte pra numero
 
-    console.log(marioPosition);
+  console.log(marioPosition);
 
-  
-    if(pipePosition <= 120 && pipePosition  > 0 && marioPosition  < 80){
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-        
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
 
+    mario.src = "img/jogo/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+    fim.src = "img/jogo/fim.jpg";
 
-        mario.src = 'img/jogo/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
-        fim.src = 'img/jogo/fim.jpg';
+    clearInterval(loop);
+  }
+}, 10);
 
-       
-
-        clearInterval(loop);
-    }
-  
-}, 10)
-
-
-document.addEventListener('keydown', jump);
+document.addEventListener("keydown", jump);
