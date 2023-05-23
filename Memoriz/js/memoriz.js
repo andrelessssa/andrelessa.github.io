67addb2bfd144f98e5f7198700515bb2a8c2fe93
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-05-23 01:34:10 -0300

    code
    coe

--- Memoriz/js/memoriz.js
+++ Memoriz/js/memoriz.js
@@ -1,12 +1,3 @@
-function clickMenu(){
-    if (itens.style.display == 'block'){
-        itens.style.display = 'none';
-        
-        
 
-    }else{
-        itens.style.display = 'block'
-    }
-    }
 
 
