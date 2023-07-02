e50dc465816359c7f5501f3f754a4e826c984129
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-07-02 16:59:04 -0300

    teste
    teste

---
+++ Gestao/src/main/java/com/gestaofesta/gestaofesta/GestaofestaApplication.java
@@ -0,0 +1,13 @@
+package com.gestaofesta.gestaofesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaofestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaofestaApplication.class, args);
+	}
+
+}
---
+++ Gestao/src/test/java/com/gestaofesta/gestaofesta/GestaofestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaofesta.gestaofesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaofestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
