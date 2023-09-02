4384656a1e9b0059cefe6d872bbd4cbd5086090a
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-09-02 12:48:10 -0300

    css
    resolvido o css

---
+++ Validacao-JS/script.js
@@ -0,0 +1,10 @@
+// primeiro selecionso todos os elementos
+
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const jobSelect = document.querySelector ("#job")
+const messageTextarea = document.querySelector("#message")
+
+console.log(form, nameInput, emailInput, passwordInput,jobSelect, messageTextarea);
